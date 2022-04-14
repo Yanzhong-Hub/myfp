57af68864bf491494e9990f287e5700488074264
Yanzhong <yanzhong.huang@outlook.com>
2022-04-14 10:03:21 +0800

    解决pie乱码

--- Apps/portfolio_stimulation/portfolio_stimulation.py
+++ Apps/portfolio_stimulation/portfolio_stimulation.py
@@ -9,10 +9,23 @@ import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
+import platform
 
 from MyPackages.DataProcess.DataProcess import DataProcess
 
-plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']  # for mac
+
+
+# 中文字体
+import matplotlib.font_manager as fm
+
+if platform.system() == 'Windows':
+    fm.findSystemFonts(fontpaths=None, fontext="ttf")
+    fm.findfont("simhei")  # for windows
+    plt.rcParams['font.sans-serif'] = ['simhei']  # for windows
+
+elif platform.system() == 'Darwin':
+    plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']  # for mac
+
 # 实例化module
 dp = DataProcess()
 
